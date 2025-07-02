246a25c007308e6b42542ba8f1307c23a3fa65e4
Gabriel <saplemtr@gmail.com>
2025-07-02 15:51:05 -0300

    f

--- routes/dropboxToken.js
+++ routes/dropboxToken.js
@@ -1,19 +1,30 @@
 const axios = require('axios');
 
 async function getAccessToken() {
-  const response = await axios.post('https://api.dropboxapi.com/oauth2/token', null, {
-    params: {
-      grant_type: 'refresh_token',
-      refresh_token: process.env.DROPBOX_REFRESH_TOKEN,
-      client_id: process.env.DROPBOX_CLIENT_ID,
-      client_secret: process.env.DROPBOX_CLIENT_SECRET
-    },
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  });
+  try {
+    const response = await axios.post('https://api.dropboxapi.com/oauth2/token', null, {
+      params: {
+        grant_type: 'refresh_token',
+        refresh_token: process.env.DROPBOX_REFRESH_TOKEN,
+        client_id: process.env.DROPBOX_CLIENT_ID,
+        client_secret: process.env.DROPBOX_CLIENT_SECRET
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 
-  return response.data.access_token;
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Erro ao obter token do Dropbox:');
+    if (error.response) {
+      console.error('Status:', error.response.status);
+      console.error('Dados:', JSON.stringify(error.response.data, null, 2));
+    } else {
+      console.error(error.message);
+    }
+    throw new Error('Erro ao gerar token do Dropbox');
+  }
 }
 
 module.exports = { getAccessToken };
